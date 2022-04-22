e9ac522f5777e33752bd1ab4e74ba5bfddc14ca1
Jakub Piga <jakub.piga.cp@gmail.com>
2022-04-22 20:01:31 +0200

    Subscribe and publish topics with MQTT functionality
    Functionality added to PasswortReset page for now. Carefull with react auto reload of the page after save - breaking socket connection. It is
     necessary to refresh the page for 100% working functionality.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Firebase from "./Pages/Firebase";
 import UserProvider from "./providers/UserProvider";
-import { Connector } from "mqtt-react-hooks";
 import "./App.css";
 
 function App() {
   return (
-    <Connector brokerUrl="wss://test.mosquitto.org:1884">
-      <UserProvider>
-        <Firebase />
-      </UserProvider>
-    </Connector>
+    <UserProvider>
+      <Firebase />
+    </UserProvider>
   );
 }
 
--- src/components/Navbar/consts/navbarItems.js
+++ src/components/Navbar/consts/navbarItems.js
@@ -1,32 +1,38 @@
-
-import AssessmentIcon from '@mui/icons-material/Assessment';
-import Inventory2Icon from '@mui/icons-material/Inventory2';
-import SettingsInputCompositeIcon from '@mui/icons-material/SettingsInputComposite';
-import SsidChartIcon from '@mui/icons-material/SsidChart';
-import * as ROUTES from '../../common/RouterContstants/routes';
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import SettingsInputCompositeIcon from "@mui/icons-material/SettingsInputComposite";
+import SsidChartIcon from "@mui/icons-material/SsidChart";
+import * as ROUTES from "../../common/RouterContstants/routes";
 export const mainNavbarItems = [
-	{
-		id: 0,
-		icon: <AssessmentIcon />,
-		label: 'Reports',
-		route: ROUTES.REPORTS,
-	},
-	{
-		id: 1,
-		icon: <SsidChartIcon />,
-		label: 'Overview',
-		route: ROUTES.OVERVIEW,
-	},
-	{
-		id: 2,
-		icon: <Inventory2Icon />,
-		label: 'Orders',
-		route: ROUTES.ORDERS,
-	},
-	{
-		id: 3,
-		icon: <SettingsInputCompositeIcon />,
-		label: 'Components',
-		route: ROUTES.COMPONENTS,
-	},
-]
+  {
+    id: 0,
+    icon: <AssessmentIcon />,
+    label: "Reports",
+    route: ROUTES.REPORTS,
+  },
+
+  {
+    id: 1,
+    icon: <SsidChartIcon />,
+    label: "Overview",
+    route: ROUTES.OVERVIEW,
+  },
+  {
+    id: 2,
+    icon: <Inventory2Icon />,
+    label: "Orders",
+    route: ROUTES.ORDERS,
+  },
+  {
+    id: 3,
+    icon: <SettingsInputCompositeIcon />,
+    label: "Components",
+    route: ROUTES.COMPONENTS,
+  },
+  {
+    id: 4,
+    icon: <AssessmentIcon />,
+    label: "Reset Password",
+    route: ROUTES.PASSWORD_RESET,
+  },
+];
