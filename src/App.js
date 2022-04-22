2a10fa5677f443f4dd47a59e2d3f1116fff78d21
Jakub Piga <jakub.piga.cp@gmail.com>
2022-04-22 04:03:53 +0200

    Fixed packages for MQTT support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Firebase from "./Pages/Firebase";
 import UserProvider from "./providers/UserProvider";
+import { Connector } from "mqtt-react-hooks";
 import "./App.css";
 
 function App() {
   return (
-    <UserProvider>
-      <Firebase />
-    </UserProvider>
+    <Connector brokerUrl="wss://test.mosquitto.org:1884">
+      <UserProvider>
+        <Firebase />
+      </UserProvider>
+    </Connector>
   );
 }
 
