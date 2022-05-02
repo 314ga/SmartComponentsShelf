322b86a06c294a06981190168358cf5cd1c0ff1f
Jakub Piga <jakub.piga.cp@gmail.com>
2022-05-02 19:15:50 +0200

    Added hardcoded ordering system

--- src/Pages/Components.js
+++ src/Pages/Components.js
@@ -1,114 +1,103 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import CommonButton from '../components/common/CommonButton';
-import BasicCard from '../components/common/BasicCard';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import GridWrapper from '../components/common/GridWrapper';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-const Components = () => {
-	const cardHeaderStyles = {
-		wrapper: {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'space-between',
-			paddingLeft: '20px',
-			paddingRight: '20px',
-			height: '65px',
-			backgroundColor: '#f5f5f5',
-			borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-		},
-		addUserButton: {
-			fontSize: '1.05rem',
-		},
-	}
-	const buttonStyles = {
-		fontSize: '0.875rem',
-		fontWeight: 600,
-		textTransform: 'capitalize',
-		borderRadius: 2.5,
-		'&.MuiButton-contained': {
-			backgroundColor: '#009be5',
-			'&:hover': {
-				backgroundColor: '#006db3'
-			},
-		},
-		'&.MuiButton-outlined': {
-			color: "#fff",
-			borderColor: '#fff',
-			'&:hover': {
-				backgroundColor: 'transparent'
-			},
-		},
-	};
-	const getHeader = () => {
-		const handleChange = (value) => {
-			console.log(value);
-		};
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
-		const addUser = () => {
-			console.log('click')
-		};
-
-		return (
-			<Box sx={cardHeaderStyles.wrapper}>
-				{/* <SearchBar
-					placeholder="Search by email address, phone number, or user UID"
-					onChange={(event) => handleChange(event.target.value)}
-					searchBarWidth='720px'
-				/> */}
-
-
-				<CommonButton
-					sx={buttonStyles}
-					variant="contained"
-				>
-					Add user
-				</CommonButton>
-				<CommonButton
-					sx={buttonStyles}
-					variant="outlined"
-				>
-					Add user
-				</CommonButton>
-
-				<Box>
-					<CommonButton
-						variant="contained"
-						onClick={addUser}
-						size="large"
-						sx={cardHeaderStyles.addUserButton}
-					>
-						Add user
-					</CommonButton>
-					<IconButton>
-						<RefreshIcon />
-					</IconButton>
-				</Box>
-			</Box>
-		)
-	};
-
-	const getContent = () => (
-		<Typography
-			align="center"
-			sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem' }}
-		>
-			No users for this project yet
-		</Typography>
-	);
-
-	return (
-		<GridWrapper>
-			<BasicCard
-				header={getHeader()}
-				content={getContent()}
-			/>
-		</GridWrapper>
-	)
+function createData(
+  component,
+  threshold,
+  orderTime,
+  autOrd,
+  status,
+  ordStatus
+) {
+  return { component, threshold, orderTime, autOrd, status, ordStatus };
 }
 
-export default Components;	
+const rows = [
+  createData("Pens", 50, 3, -1, "GOOD", "Last order: 20.04.2022"),
+  createData(
+    "Screwdrivers",
+    10,
+    5,
+    5,
+    "ORDER RECOMENDED",
+    "Last order: 29.04.2022"
+  ),
+  createData(
+    "Capacitors",
+    15,
+    10,
+    4,
+    "ORDERED",
+    "Expected delivery: 03.05.2022"
+  ),
+];
+
+const Components = () => {
+  return (
+    <TableContainer
+      component={Paper}
+      style={{ marginLeft: "400px", marginRight: "100px" }}
+    >
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Component</StyledTableCell>
+            <StyledTableCell align="right">Threshold (pieces)</StyledTableCell>
+            <StyledTableCell align="right">OrderTime (days)</StyledTableCell>
+            <StyledTableCell align="right">
+              Automatic ordering (When x pieces left)
+            </StyledTableCell>
+            <StyledTableCell align="right">Status</StyledTableCell>
+            <StyledTableCell align="right">Order Status</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.component}>
+              <StyledTableCell component="th" scope="row">
+                {row.component}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.threshold}</StyledTableCell>
+              <StyledTableCell align="right">{row.orderTime}</StyledTableCell>
+              <StyledTableCell align="right">
+                {row.autOrd > -1 ? row.autOrd : "OFF"}
+              </StyledTableCell>
+
+              <StyledTableCell align="right">{row.status}</StyledTableCell>
+              <StyledTableCell align="right">{row.ordStatus}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Components;
